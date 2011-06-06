0a34075c80411eec2b752c904ad25db603573c3e
Dave Lowe <mahalowe@gmail.com>
2011-06-06 12:55:40 -0500

    Fixes for mailchimp backend

--- templated_email/backends/mailchimp_sts.py
+++ templated_email/backends/mailchimp_sts.py
@@ -1,12 +1,14 @@
 import django_vanilla
 from mailsnake import MailSnakeSTS
 from django.conf import settings
+from django.utils.translation import ugettext as _
 
-class TemplateBackend(django_vanilla.TemplateBackend):
-    def __init__(self, *args, **kwargs):
+class TemplateBackend(vanilla_django.TemplateBackend):
+    def __init__(self, fail_silently=False, template_prefix='templated_email/', *args, **kwargs):
+        self.template_prefix = template_prefix
         self.connection = MailSnakeSTS(apikey = settings.MAILCHIMP_API_KEY)
         
-    def send(self, template_name, from_email, recipient_list, context, fail_silently=False, headers={}):
+    def send(self, template_name, from_email, to_email, context, fail_silently=False, headers={}):
         config = getattr(settings,'TEMPLATED_EMAIL_MAILCHIMP',{}).get(template_name,{})
         parts = self._render_email(template_name, context)
         params={
@@ -16,7 +18,7 @@ class TemplateBackend(django_vanilla.TemplateBackend):
                 'text':parts.get('plain',''),
                 'from_name':' '.join(from_email.split(' ')[:-1]) or 'Nobody',
                 'from_email':from_email,
-                'to_email':recipient_list,
+                'to_email':to_email,
             },
             'track_opens':config.get('track_opens',False),
             'track_clicks':config.get('track_clicks',False),
